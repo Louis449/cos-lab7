09543850c695cad59ac4fda232872dd51fe0dd49
Louis449 <106399199@student.swin.edu.au>
2026-05-29 17:30:04 +0800

    Modified cars.php to include table for each query

--- Exercise1/cars.php
+++ Exercise1/cars.php
@@ -8,6 +8,7 @@
 <body>
     <h1>Query Results from the Connected Database</h1>
 
+    <h2>First Query Results</h2>
     <table border="1" cellpadding="5">
         <tr>
             <th>ID</th>
@@ -19,46 +20,133 @@
     <?php
         require_once "../settings.php";
         $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
         if ($dbconn) {
-            // $query = "SELECT * FROM cars";
-            // $query = "SELECT make, model, price FROM cars ORDER BY make, model";
-            // $query = "SELECT make, model FROM cars WHERE price >= 20000";
-            // $query = "SELECT make, AVG(price) FROM cars GROUP BY make";
+            $query = "SELECT * FROM cars";
+            $result = mysqli_query($dbconn, $query);
+            
+            if ($result) {                
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" .$row['car_id'] ."</td>";
+                    echo "<td>" .$row['make'] ."</td>";
+                    echo "<td>" .$row['model'] ."</td>";
+                    echo "<td>" .$row['price'] ."</td>";
+                    echo "<td>" .$row['yom'] ."</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<p> There are no cars to display</p>";
+            }
+            // mysqli_close($dbconn);
+        } else {
+            echo "<p>Unable to connect to the db.</p>";
+        }
+    ?>
+    </table>
+
+    <h2>Second Query Results</h2>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Price</th>
+        </tr>
+    <?php
+        if ($dbconn) {
+            $query = "SELECT make, model, price FROM cars ORDER BY make, model";
+            $result = mysqli_query($dbconn, $query);
+            
+            if ($result) {                
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" .$row['make'] ."</td>";
+                    echo "<td>" .$row['model'] ."</td>";
+                    echo "<td>" .$row['price'] ."</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<p> There are no cars to display</p>";
+            }
+            // mysqli_close($dbconn);
+        } else {
+            echo "<p>Unable to connect to the db.</p>";
+        }
+    ?>
+    </table>
+
+    <h2>Third Query Results</h2>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Make</th>
+            <th>Model</th>
+        </tr>
+    <?php
+        if ($dbconn) {
+            $query = "SELECT make, model FROM cars WHERE price >= 20000";
+            $result = mysqli_query($dbconn, $query);
+            
+            if ($result) {                
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" .$row['make'] ."</td>";
+                    echo "<td>" .$row['model'] ."</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<p> There are no cars to display</p>";
+            }
+            // mysqli_close($dbconn);
+        } else {
+            echo "<p>Unable to connect to the db.</p>";
+        }
+    ?>
+    </table>
+
+    <h2>Fourth Query Results</h2>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Make</th>
+            <th>Price</th>
+        </tr>
+    <?php
+        if ($dbconn) {
+            $query = "SELECT make, AVG(price) FROM cars GROUP BY make";
+            $result = mysqli_query($dbconn, $query);
+            
+            if ($result) {                
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" .$row['make'] ."</td>";
+                    echo "<td>" .$row['AVG(price)'] ."</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<p> There are no cars to display</p>";
+            }
+            // mysqli_close($dbconn);
+        } else {
+            echo "<p>Unable to connect to the db.</p>";
+        }
+    ?>
+    </table>
+
+    <h2>Fifth Query Results</h2>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Make</th>
+            <th>Model</th>
+        </tr>
+    <?php
+        if ($dbconn) {
             $query = "SELECT make, model FROM cars WHERE price < 15000 AND price > 10000";
             $result = mysqli_query($dbconn, $query);
             
             if ($result) {                
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "<tr>";
-                    if (array_key_exists('car_id', $row)) {
-                        echo "<td>" .$row['car_id'] ."</td>";
-                    } else {
-                        echo "<td></td>";
-                    }
-
-                    if (array_key_exists('make', $row)) {
-                        echo "<td>" .$row['make'] ."</td>";
-                    } else {
-                        echo "<td></td>";
-                    }
-
-                    if (array_key_exists('model', $row)) {
-                        echo "<td>" .$row['model'] ."</td>";
-                    } else {
-                        echo "<td></td>";
-                    }
-
-                    if (array_key_exists('price', $row)) {
-                        echo "<td>" .$row['price'] ."</td>";
-                    } else {
-                        echo "<td></td>";
-                    }
-
-                    if (array_key_exists('yom', $row)) {
-                        echo "<td>" .$row['yom'] ."</td>";
-                    } else {
-                        echo "<td></td>";
-                    }
+                    echo "<td>" .$row['make'] ."</td>";
+                    echo "<td>" .$row['model'] ."</td>";
                     echo "</tr>";
                 }
             } else {
